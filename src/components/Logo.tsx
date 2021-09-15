3d753d5de3204f3e4f125bfb341d510f2da62988
Aaron Carlino <unclecheese@leftandmain.com>
2021-09-16 10:50:57 +1200

    Use Silverstripe CMS branding

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,11 +7,11 @@ const Logo = (): JSX.Element => {
     <div className="flex justify-start lg:w-0 lg:flex-1">
       <Link href={`/`}>
           <a>
-        <span className="sr-only">SS + NextJS</span>
+        <span className="sr-only">Silverstripe CMS + NextJS</span>
         <div className="flex items-center">
           <GlobeIcon className="h-8 w-auto sm:h-10 text-gray-400" />
           <span className="mx-2 font-bold">
-            Your Silverstripe + NextJS website
+            Your Silverstripe CMS + NextJS website
           </span>
         </div>
         </a>
